2c5515a2ca4b6e4b562cbe475925ebdb9f8a409b
Sacramentovich <38147948+Jaisac@users.noreply.github.com>
2018-06-20 22:10:51 -0500

    Update Pixel_convert.cpp

--- Pixel_convert.cpp
+++ Pixel_convert.cpp
@@ -1,3 +1,14 @@
+/*
+ * 
+ *
+ *  Created on: 23/07/2016
+ *      Author: Jaime Sacramento
+ *	Description: Convert the color of the pixels of an image according to a 
+ *	imposed threshold.
+ *	
+ */
+
+
 #include <opencv.hpp>
 #include <imgproc.hpp>
 #include <highgui.hpp>
@@ -15,26 +26,28 @@ using namespace std;
 
 int main()
 {
-
 	int pixel;
 	Mat img ;
 	Mat img2;
+	threshold = 110;
+	
 	img2 = imread("C:\\Users\\jamessacramento\\Pictures\\lab_imagenes\\opencv\\megumin_face.jpg", CV_LOAD_IMAGE_COLOR);
 	 namedWindow("Miventana", CV_WINDOW_NORMAL);
 	 resizeWindow("Miventana",420,520);
-	//Conversión de color
+//########## Color to gray routine
 	cv::cvtColor(img2, img,COLOR_BGR2GRAY);
-	//Guarda la imagen en una carpeta
-	cv::imwrite("C:\\Users\\jamessacramento\\Pictures\\lab_imagenes\\opencv\\Megumin_face2.png",img);
-//	img = imread("C:\\Users\\jamessacramento\\Pictures\\lab_imagenes\\opencv\\Megumin_face2.png");
+//########## Saves the image in hardrive
+	//Note: The image's path must be expressed here i.e C//myimages//images
+	cv::imwrite("path",img);
 
-	//Mapeo de la imagen para identificar coordenada y valor en escala de grizes del pixel
+//########## Mapping to identify the coordinate and the pixel value of every pixel
 	std::ofstream imagen;
 	imagen.open("megumin.txt");
+	
 	 for(int i=0;i < img.cols;i++){
 		 for (int j=0;j < img.rows;j++){
 
-			 if (img.at<uchar>(j, i) > 110){
+			 if (img.at<uchar>(j, i) > threshold){
 				 img.at<uchar>(j, i) = 0;
 				 pixel = (int)img.at<uchar>(j, i);
 			 }
